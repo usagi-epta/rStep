bc340e12508a783aee3e88cb6daf50931d0a164a
thereza <thereza@69d7e21a-02f2-0a07-4c3c-73c1840f7892>
2010-12-19 23:31:19 +0000

    cleaned up messages.h file

--- firmware/rStep/messages.h
+++ firmware/rStep/messages.h
@@ -1,6 +1,26 @@
-#define Message1(msg,value) Serial.print("MSG ");Serial.print(msg);Serial.print('(');Serial.print(value);Serial.println(')')
-#define Message1F(msg,value,format) Serial.print("MSG ");Serial.print(msg);Serial.print('(');Serial.print(value, format);Serial.println(')')
-#define Message3F(msg,x,y,z,format) Serial.print("MSG ");Serial.print(msg);Serial.print('(');Serial.print(x, format);Serial.print(',');Serial.print(y, format);Serial.print(',');Serial.print(z, format);Serial.println(')')
+#ifndef __MESSAGES_H_
+#define __MESSAGES_H_
 
+#define Message1(msg,value) 	Serial.print("MSG "); \
+				Serial.print(msg);\
+				Serial.print('(');\
+				Serial.print(value);\
+				Serial.println(')')
+#define Message1F(msg,value,format) 	Serial.print("MSG ");\
+					Serial.print(msg);\
+					Serial.print('(');\
+					Serial.print(value, format);\
+					Serial.println(')')
+#define Message3F(msg,x,y,z,format) 	Serial.print("MSG ");\
+					Serial.print(msg);\
+					Serial.print('(');
+					Serial.print(x, format);
+					Serial.print(',');
+					Serial.print(y, format);
+					Serial.print(',');
+					Serial.print(z, format);
+					Serial.println(')')
 #define MessageCoordinates(x,y,z) Message3F("Coord",x,y,z,5)
+
+#endif
 
